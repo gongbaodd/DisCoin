680fd01cfd504d2c733871724355d0813d70222e
Yiqi-H <144740550+Yiqi-H@users.noreply.github.com>
2025-01-25 22:23:07 +0200

    Revert "Merge branch 'main' of https://github.com/gongbaodd/DisCoin"
    This reverts commit 33476c0a2a282d69536da80ac8a1a7c51cf40b65, reversing
    changes made to 78c239d6c41ae61f245f412e27b850c18071190a.

--- DisCoin-React/src/App.tsx
+++ DisCoin-React/src/App.tsx
@@ -2,25 +2,18 @@ import './App.css'
 import { Unity, useUnityContext } from "react-unity-webgl"
 
 function App() {
-  const { unityProvider, requestFullscreen  } = useUnityContext({
+  const { unityProvider } = useUnityContext({
     loaderUrl: "Build/Build.loader.js",
     dataUrl: "Build/Build.data.br",
     frameworkUrl: "Build/Build.framework.js.br",
     codeUrl: "Build/Build.wasm.br",
   });
 
-  const onRequestFullscreen = () => {
-    requestFullscreen(true);
-  }
-
   return (
     <>
       <h1>DisCoin: The Crypto Bubble Simulator</h1>
       <div className="card">
         <Unity unityProvider={unityProvider} style={{ width: 960, height: 600 }} />
-      </div>
-      <div className="card">
-        <button onClick={onRequestFullscreen}>Fullscreen</button>
       </div>
     </>
   )
--- DisCoin-Unity/Assets/Scripts/GameManager.cs
+++ DisCoin-Unity/Assets/Scripts/GameManager.cs
@@ -3,8 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using TMPro;
-using UnityEditor.SearchService;
-using UnityEngine.SceneManagement;
 
 
 /*
@@ -75,7 +73,8 @@ public class GameManager : MonoBehaviour
         }
 
          CoinValueText.text = "$0";
-         SceneManager.LoadScene("GameOver");
+
+         //TODO: Game Over
     }
 
     // Update is called once per frame
