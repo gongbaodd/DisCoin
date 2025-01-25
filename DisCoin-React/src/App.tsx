3d319b4e2908c94b946c259b8ad492d2c76656fa
gongbaodd@ProArt <gongbaodd@qq.com>
2025-01-25 21:53:48 +0200

    add fullscreen button

--- DisCoin-React/src/App.tsx
+++ DisCoin-React/src/App.tsx
@@ -2,18 +2,25 @@ import './App.css'
 import { Unity, useUnityContext } from "react-unity-webgl"
 
 function App() {
-  const { unityProvider } = useUnityContext({
+  const { unityProvider, requestFullscreen  } = useUnityContext({
     loaderUrl: "Build/Build.loader.js",
     dataUrl: "Build/Build.data.br",
     frameworkUrl: "Build/Build.framework.js.br",
     codeUrl: "Build/Build.wasm.br",
   });
 
+  const onRequestFullscreen = () => {
+    requestFullscreen(true);
+  }
+
   return (
     <>
       <h1>DisCoin: The Crypto Bubble Simulator</h1>
       <div className="card">
         <Unity unityProvider={unityProvider} style={{ width: 960, height: 600 }} />
+      </div>
+      <div className="card">
+        <button onClick={onRequestFullscreen}>Fullscreen</button>
       </div>
     </>
   )
