a7d1f3ba01fd867e5833310dcc6c38211bbed8aa
gongbaodd@ProArt <gongbaodd@qq.com>
2025-01-26 13:00:47 +0200

    feat: add decision sprites and update DecisionCardController logic

--- DisCoin-Unity/Assets/Scripts/Controllers/DecisionCardController.cs
+++ DisCoin-Unity/Assets/Scripts/Controllers/DecisionCardController.cs
@@ -1,13 +1,22 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 
 public class DecisionCardController : MonoBehaviour
 {
     public TMP_Text textMeshPro;
     private GameManager gameManager;
+ 
+    [SerializeField] private GameObject label;
 
     [SerializeField] private DecisionModel decision;
+
+    [SerializeField] private Sprite dismaySprite;
+    [SerializeField] private Sprite dismissSprite;
+    [SerializeField] private Sprite distortSprite;
+    [SerializeField] private Sprite distractSprite;
+    [SerializeField] private Sprite truthSprite;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,17 +24,30 @@ public class DecisionCardController : MonoBehaviour
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     
     public void SetDecision(DecisionModel decision)
     {
         this.decision = decision;
         textMeshPro.text = decision.content;
+
+        switch (decision.label)
+        {
+            case DecisionType.DISMAY:
+                label.GetComponent<Image>().sprite = dismaySprite;
+                break;
+            case DecisionType.DISMISS:
+                label.GetComponent<Image>().sprite = dismissSprite;
+                break;
+            case DecisionType.DISTORT:
+                label.GetComponent<Image>().sprite = distortSprite;
+                break;
+            case DecisionType.DISTRACT:
+                label.GetComponent<Image>().sprite = distractSprite;
+                break;
+            case DecisionType.TRUTH:
+                label.GetComponent<Image>().sprite = truthSprite;
+                break;
+        }
     }
 
     public void OnClick()
