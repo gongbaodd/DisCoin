f7926f12b2144255f5d6647514d8462876f08be7
MartinWachira <67173409+mmwachira@users.noreply.github.com>
2025-02-07 20:17:41 +0200

    feat: add continue button

--- DisCoin-Unity/Assets/Scripts/Controllers/GameStartController.cs
+++ DisCoin-Unity/Assets/Scripts/Controllers/GameStartController.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameStartController : MonoBehaviour
 {
@@ -9,6 +11,9 @@ public class GameStartController : MonoBehaviour
     public Transform contentContainer;
     public Message[] messages;
     public float initialDelay = 1f;
+
+    public ScrollRect scrollRect;
+    public GameObject continueButton;
 
     private void Start()
     {
@@ -33,7 +38,8 @@ public class GameStartController : MonoBehaviour
             TMP_Text bubbleText = newBubble.GetComponentInChildren<TMP_Text>();
             bubbleText.text = msg.messageText;
 
-            messageBubblePrefab.SetActive(true);
+            //messageBubblePrefab.SetActive(true);
+            newBubble.SetActive(true);
 
 
             RectTransform bubbleTransform = newBubble.GetComponent<RectTransform>();
@@ -46,30 +52,38 @@ public class GameStartController : MonoBehaviour
                 bubbleTransform.anchorMax = new Vector2(1, 0);
                 bubbleTransform.pivot = new Vector2(1, 0.5f);
                 bubbleTransform.anchoredPosition = new Vector2(-containerTransform.rect.width, bubbleTransform.anchoredPosition.y);
-
-
                 bubbleTransform.localScale = new Vector3(-1, 1, 1);
-
-
                 bubbleText.rectTransform.localScale = new Vector3(-1, 1, 1);
+            }
+
+            // Wait one frame to allow the layout system to update the content size.
+            yield return null;
+
+            // Force an immediate layout rebuild to ensure new bubble's size is accounted for.
+            Canvas.ForceUpdateCanvases();
+
+            // Scroll to the bottom (verticalNormalizedPosition = 0 means bottom).
+            if (scrollRect != null)
+            {
+                scrollRect.verticalNormalizedPosition = 0f;
             }
 
 
             yield return new WaitForSeconds(msg.delayBeforeNext);
         }
 
+        continueButton.SetActive(true);
 
-        StartGame();
     }
 
-    private void StartGame()
+    public void StartGame()
     {
         sceneTransition.TransitionScene();
     }
 
     public void QuitGame()
     {
-        Application.Quit();
+        SceneManager.LoadScene("IntroCutscene");
     }
 }
 
